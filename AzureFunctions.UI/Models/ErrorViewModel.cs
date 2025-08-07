37b6265805d80a6c36b1d0615a7e7c06ee3bbffa
Manuel Naranjo <manuelnaranjo@outlook.com>
2025-08-06 22:45:01 -0600

    create UI project

---
+++ AzureFunctions.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AzureFunctions.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
