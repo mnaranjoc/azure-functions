1db755065a2af19a290bf80f978aec6a7377d167
Manuel Naranjo <manuelnaranjo@outlook.com>
2025-08-06 23:46:05 -0600

    create user registry form and model

---
+++ AzureFunctions.UI/Models/UserModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AzureFunctions.UI.Models
+{
+    public class UserModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string? Name { get; set; }
+
+        [Required]
+        public string? Email { get; set; }
+
+        [Required]
+        public string? Phone { get; set; }
+
+        public string? Status { get; set; }
+    }
+}
