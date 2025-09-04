086d352a939250c7ab2dc280919a9e7071c8f1d0
mnaranjoc <manuelnaranjo@outlook.com>
2025-09-04 13:14:34 -0600

    store queue messages in sql

--- AzureFunctions.Functions/OnQueueTriggerUpdateDatabase.cs
+++ AzureFunctions.Functions/OnQueueTriggerUpdateDatabase.cs
@@ -1,22 +1,43 @@
 using System;
 using Azure.Storage.Queues.Models;
+using AzureFunctions.Functions.Data;
+using AzureFunctions.UI.Models;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace AzureFunctions.Functions;
 
 public class OnQueueTriggerUpdateDatabase
 {
     private readonly ILogger<OnQueueTriggerUpdateDatabase> _logger;
+    private readonly ApplicationDbContext _dbContext;
 
-    public OnQueueTriggerUpdateDatabase(ILogger<OnQueueTriggerUpdateDatabase> logger)
+    public OnQueueTriggerUpdateDatabase(ILogger<OnQueueTriggerUpdateDatabase> logger,
+        ApplicationDbContext dbContext)
     {
         _logger = logger;
+        _dbContext = dbContext;
     }
 
     [Function(nameof(OnQueueTriggerUpdateDatabase))]
     public void Run([QueueTrigger("AzureQueueTest", Connection = "")] QueueMessage message)
     {
-        _logger.LogInformation("------------Queue message received------------- {messageText}", message.MessageText);
+        var messageBody = message.Body.ToString();
+        var user = JsonConvert.DeserializeObject<UserModel>(messageBody);
+
+        if (user != null)
+        {
+            user.Id = 1;
+            user.Status = "active";
+            _dbContext.Users.Add(user);
+            _dbContext.SaveChanges();
+
+            _logger.LogInformation("User stored successfully");
+        }
+        else
+        {
+            _logger.LogWarning("Failed to deserialize user object.");
+        }
     }
 }
--- AzureFunctions.Functions/OnUserRegisterWriteToQueue.cs
+++ AzureFunctions.Functions/OnUserRegisterWriteToQueue.cs
@@ -1,7 +1,9 @@
+using AzureFunctions.UI.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace AzureFunctions.Functions;
 
@@ -16,11 +18,12 @@ public class OnUserRegisterWriteToQueue
 
     [Function("OnUserRegisterWriteToQueue")]
     [QueueOutput("AzureQueueTest")]
-    public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
+    public async Task<UserModel> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
     {
         string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+        var user = JsonConvert.DeserializeObject<UserModel>(requestBody);
 
         _logger.LogInformation("---------Request body stored in queue.---------");
-        return new OkObjectResult(requestBody);
+        return user ?? new UserModel();
     }
 }
